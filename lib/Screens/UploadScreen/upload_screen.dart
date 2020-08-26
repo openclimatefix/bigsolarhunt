cb66c9fe1609f5d0c7dfae036d0a16ac4a0e8d26
solomoncotton <solcotton@live.com>
2020-08-26 13:14:43 +0100

    Added map to upload screen

--- lib/Screens/UploadScreen/upload_screen.dart
+++ lib/Screens/UploadScreen/upload_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 import 'UploadScreenWidgets/upload_button.dart';
 import 'UploadScreenWidgets/image_card.dart';
@@ -47,7 +49,7 @@ class _UploadScreenState extends State<UploadScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Upload a photo'),
+        title: Text('Upload a photo', style: Theme.of(context).textTheme.headline4),
         actions: <Widget>[
           IconButton(
               icon: const Icon(Icons.refresh), onPressed: () => _regetImage())
@@ -59,7 +61,7 @@ class _UploadScreenState extends State<UploadScreen> {
         Container(
             // TODO: Map goes here
             color: Colors.blue,
-            child: Center(child: Text('Current location on map'))),
+            child: MapWithDraggablePin()),
         Container(
             padding: EdgeInsets.all(20),
             alignment: Alignment.topCenter,
@@ -67,8 +69,76 @@ class _UploadScreenState extends State<UploadScreen> {
         Container(
             padding: EdgeInsets.all(20),
             alignment: Alignment.bottomCenter,
-            child: UploadButton(image: _image))
+            child: UploadButton(image: _image)),
+        Center(
+          child: Icon(
+            Icons.control_point_duplicate,
+            color: Colors.white,
+            size: 50,
+          ),
+        )
       ])),
+    );
+  }
+}
+
+class MapWithDraggablePin extends StatefulWidget {
+  @override
+  _MapWithDraggablePinState createState() => _MapWithDraggablePinState();
+}
+
+class _MapWithDraggablePinState extends State<MapWithDraggablePin> {
+  Location location = new Location();
+  GoogleMapController _mapController;
+  static LatLng _markerLocation;
+
+  _getUserLocation() async {
+    LocationData locationData = await location.getLocation();
+    setState(() {
+      _markerLocation = LatLng(locationData.latitude, locationData.longitude);
+    });
+  }
+
+  _onCameraIdle() async {
+    _
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserLocation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+      mapType: MapType.satellite,
+      minMaxZoomPreference: MinMaxZoomPreference(18, 20),
+      myLocationEnabled: true,
+      myLocationButtonEnabled: true,
+      initialCameraPosition: CameraPosition(
+        target: _markerLocation,
+        zoom: 19,
+      ),
+      markers: Set<Marker>.of(
+        <Marker>[
+          Marker(
+              onTap: () {
+                print('Tapped');
+              },
+              draggable: true,
+              markerId: MarkerId('Marker'),
+              position: _markerLocation,
+              onDragEnd: ((value) {
+                print(value.latitude);
+                print(value.longitude);
+              }))
+        ],
+      ),
+      onMapCreated: (GoogleMapController controller) {
+        _mapController = controller;
+      },
+      onCameraIdle: () => print(_mapController.getLatLng())),
     );
   }
 }
