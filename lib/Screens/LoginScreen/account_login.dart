2dc45d547a18cacc741fd5f4ec68366f6a6944f7
solomoncotton <solcotton@live.com>
2021-03-16 11:00:49 +0000

    prep for release

--- lib/Screens/LoginScreen/account_login.dart
+++ lib/Screens/LoginScreen/account_login.dart
@@ -8,6 +8,36 @@ import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:solar_hunt/Services/mapillary_service.dart';
 
 class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        onWillPop: () => Navigator.of(context).pushNamed('/'),
+        child: Container(
+            constraints: BoxConstraints.expand(),
+            padding: EdgeInsets.only(top: 20, bottom: 20, left: 12, right: 12),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/login-screen-background.png"),
+                    fit: BoxFit.cover)),
+            child: Container(
+                padding: EdgeInsets.all(20),
+                height: MediaQuery.of(context).size.height * 0.7,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Padding(
+                          padding: EdgeInsets.all(20),
+                          child: Image.asset('assets/mapillary-logo.png')),
+                      CreateAccountCard()
+                    ]))));
+  }
+}
+
+class CreateAccountCard extends StatelessWidget {
+  const CreateAccountCard({
+    Key key,
+  }) : super(key: key);
+
   _continueWithoutAccount(BuildContext context) async {
     const _BEARER_TOKEN =
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtcHkiLCJzdWIiOiI3ak9MM21JMVR4dThCUkU2em9laXVDIiwiYXVkIjoiU0cxbk9HWk5XRXRtYWxGbE4yMUpiRll4UjJsdGREb3pOVGxrTURFeU4yRTVZak0xTWpReCIsImlhdCI6MTYxMTU2OTk1OTQ5NCwianRpIjoiZjI4MjUyYWQ4NzNlZmM2YzczMzU3MTIyMGUwMjg3NjYiLCJzY28iOlsidXNlcjplbWFpbCIsInVzZXI6cmVhZCIsInB1YmxpYzp1cGxvYWQiXSwidmVyIjoxfQ.K_liNbNRshillMav_gAnaKv9RjWrAbrsJPLnHK5frWg';
@@ -49,92 +79,48 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-        onWillPop: () => Navigator.of(context).pushNamed('/'),
-        child: SafeArea(
-            child: Stack(children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            decoration:
-                new BoxDecoration(color: Color.fromARGB(240, 255, 255, 255)),
-          ),
-          Positioned(
-              top: -MediaQuery.of(context).size.height * 0.9,
-              left: -MediaQuery.of(context).size.width * 0.5,
-              child: Container(
-                width: MediaQuery.of(context).size.width * 2,
-                height: MediaQuery.of(context).size.height * 2,
-                decoration: new BoxDecoration(
-                  color: Color.fromARGB(240, 33, 43, 54),
-                  shape: BoxShape.circle,
-                ),
-                alignment: Alignment.topCenter,
-              )),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Image.asset('assets/mapillary-logo.png')),
-              Container(
-                  padding: EdgeInsets.all(20),
-                  height: MediaQuery.of(context).size.height * 0.5,
-                  child: Card(
-                      child: SingleChildScrollView(
-                    child: Container(
-                      padding: EdgeInsets.all(20),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Padding(
-                                  padding: EdgeInsets.only(bottom: 10),
-                                  child: Text(
-                                    'Create an account with Mapilliary?',
-                                    style:
-                                        Theme.of(context).textTheme.headline5,
-                                    textAlign: TextAlign.center,
-                                  )),
-                              Text(
-                                'Mappilary is the service that processes and uploads submitted images to OpenStreetMap. Creating an account enables you to view your submissions on Mapillary\'s website.',
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyText2
-                                    .copyWith(color: Colors.grey),
-                                textAlign: TextAlign.center,
-                              )
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              RaisedButton(
-                                  child: Text('Create Account',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .button
-                                          .copyWith(color: Colors.white)),
-                                  color: Color.fromARGB(240, 33, 43, 54),
-                                  onPressed: () => _createAccount(context)),
-                              OutlineButton(
-                                  child: Text('Not now'),
-                                  onPressed: () =>
-                                      _continueWithoutAccount(context)),
-                              Text('(You can change your mind later)',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyText2
-                                      .copyWith(color: Colors.grey))
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  )))
-            ],
-          )
-        ])));
+    return Card(
+        child: SingleChildScrollView(
+            padding: EdgeInsets.all(12),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: Text(
+                      'Create an account with Mapilliary?',
+                      style: Theme.of(context).textTheme.headline5,
+                      textAlign: TextAlign.center,
+                    )),
+                Padding(
+                    padding: EdgeInsets.only(bottom: 20),
+                    child: Text(
+                      'Mappilary is the service that processes and uploads submitted images to OpenStreetMap. Creating an account enables you to view your submissions on Mapillary\'s website.',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText2
+                          .copyWith(color: Colors.grey),
+                      textAlign: TextAlign.center,
+                    )),
+                RaisedButton(
+                    child: Text('Create Account',
+                        style: Theme.of(context)
+                            .textTheme
+                            .button
+                            .copyWith(color: Colors.white)),
+                    color: Color.fromARGB(240, 33, 43, 54),
+                    onPressed: () => _createAccount(context)),
+                OutlineButton(
+                    child: Text('Not now'),
+                    onPressed: () => _continueWithoutAccount(context)),
+                Padding(
+                    padding: EdgeInsets.only(top: 20),
+                    child: Text('(You can change your mind later)',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText2
+                            .copyWith(color: Colors.grey)))
+              ],
+            )));
   }
 }
