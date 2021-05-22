caf0256da8927bdf19fed9428fcbcb315f270753
solomoncotton <solcotton@live.com>
2021-05-22 16:14:50 +0000

    change map handling so it works on iOS, remove login from onboarding

--- lib/Screens/MapScreen/osm_map.dart
+++ lib/Screens/MapScreen/osm_map.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
 import 'dart:ui';
+import 'dart:async';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
 import 'package:solar_hunt/Services/internet_services.dart';
-import 'package:user_location/user_location.dart';
 import 'package:solar_hunt/DataStructs/solar_panel.dart';
 import 'package:solar_hunt/Services/database_services.dart';
+import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 
-class OpenStreetMapScreen extends StatelessWidget {
+class OpenStreetMapScreen extends StatefulWidget {
+  @override
+  _OpenStreetMapScreenState createState() => _OpenStreetMapScreenState();
+}
+
+class _OpenStreetMapScreenState extends State<OpenStreetMapScreen> {
   static Widget _panelMarker;
   static Widget _uploadMarker;
   static String _tileUrl;
   static List<Marker> _markers = [];
   static DatabaseProvider panelDatabase = DatabaseProvider.databaseProvider;
-  static MapController mapController = MapController();
-  static UserLocationOptions userLocationOptions;
-
-  const OpenStreetMapScreen({
-    Key key,
-  }) : super(key: key);
+  CenterOnLocationUpdate _centerOnLocationUpdate;
+  StreamController<double> _centerCurrentLocationStreamController;
 
   _getMapData(BuildContext context) async {
     _markers = [];
@@ -32,19 +34,6 @@ class OpenStreetMapScreen extends StatelessWidget {
     List<Marker> uploadQueue = await _getUploadQueuePanels();
     _markers.addAll(userPanelData);
     _markers.addAll(uploadQueue);
-    userLocationOptions = UserLocationOptions(
-        context: context,
-        mapController: mapController,
-        markers: _markers,
-        updateMapLocationOnPositionChange: false,
-        moveToCurrentLocationFloatingActionButton: Container(
-            height: 20,
-            width: 20,
-            decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Theme.of(context).colorScheme.surface),
-            child: Icon(Icons.gps_fixed_rounded,
-                color: Theme.of(context).colorScheme.onSurface)));
   }
 
   Future<List<Marker>> _getUserPanels() async {
@@ -76,6 +65,19 @@ class OpenStreetMapScreen extends StatelessWidget {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _centerOnLocationUpdate = CenterOnLocationUpdate.always;
+    _centerCurrentLocationStreamController = StreamController<double>();
+  }
+
+  @override
+  void dispose() {
+    _centerCurrentLocationStreamController.close();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     _panelMarker = Image.asset('assets/icons/panel-icon.png');
     _uploadMarker = Image.asset('assets/icons/panel-icon-queue.png');
@@ -84,46 +86,75 @@ class OpenStreetMapScreen extends StatelessWidget {
         ? 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png'
         : 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png';
 
-    return Container(
-      color: Theme.of(context).colorScheme.background,
-      child: FutureBuilder(
-        future: _getMapData(context),
-        builder: (context, AsyncSnapshot snapshot) {
-          if (snapshot.hasData && snapshot.data == true) {
-            return Column(
-              children: [
-                Flexible(
-                  child: FlutterMap(
-                    options: MapOptions(
-                      center: LatLng(54.12501425, -4.31989979),
-                      zoom: 5.3,
-                      maxZoom: 18,
-                      plugins: [
-                        UserLocationPlugin(),
-                      ],
+    return Scaffold(
+        body: Container(
+          color: Theme.of(context).colorScheme.background,
+          child: FutureBuilder(
+            future: _getMapData(context),
+            builder: (context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData && snapshot.data == true) {
+                return Column(
+                  children: [
+                    Flexible(
+                      child: FlutterMap(
+                        options: MapOptions(
+                            center: LatLng(0, 0),
+                            zoom: 5.3,
+                            maxZoom: 18,
+                            // Stop centering the location marker on the map if user interacted with the map.
+                            onPositionChanged:
+                                (MapPosition position, bool hasGesture) {
+                              if (hasGesture) {
+                                setState(() => _centerOnLocationUpdate =
+                                    CenterOnLocationUpdate.never);
+                              }
+                            }),
+                        children: [
+                          TileLayerWidget(
+                            options: TileLayerOptions(
+                                urlTemplate: _tileUrl,
+                                subdomains: ['a', 'b', 'c'],
+                                tileProvider: NonCachingNetworkTileProvider(),
+                                retinaMode: true &&
+                                    MediaQuery.of(context).devicePixelRatio >
+                                        1.0),
+                          ),
+                          MarkerLayerWidget(
+                            options: MarkerLayerOptions(markers: _markers),
+                          ),
+                          LocationMarkerLayerWidget(
+                            plugin: LocationMarkerPlugin(
+                              centerCurrentLocationStream:
+                                  _centerCurrentLocationStreamController.stream,
+                              centerOnLocationUpdate: _centerOnLocationUpdate,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    layers: [
-                      TileLayerOptions(
-                          urlTemplate: _tileUrl,
-                          subdomains: ['a', 'b', 'c'],
-                          tileProvider: NonCachingNetworkTileProvider(),
-                          retinaMode: true &&
-                              MediaQuery.of(context).devicePixelRatio > 1.0),
-                      MarkerLayerOptions(markers: _markers),
-                      userLocationOptions,
-                    ],
-                    mapController: mapController,
-                  ),
-                ),
-              ],
-            );
-          } else if (snapshot.hasData && snapshot.data == false) {
-            return Center(child: NotConnectedContainer(showtext: true));
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-    );
+                  ],
+                );
+              } else if (snapshot.hasData && snapshot.data == false) {
+                return Center(child: NotConnectedContainer(showtext: true));
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Automatically center the location marker on the map when location updated until user interact with the map.
+            setState(
+                () => _centerOnLocationUpdate = CenterOnLocationUpdate.always);
+            // Center the location marker on the map and zoom the map to level 18.
+            _centerCurrentLocationStreamController.add(18);
+          },
+          child: Icon(
+            Icons.my_location,
+            color: Colors.white,
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat);
   }
 }
